ae22de1c5853a0199189de3408e24cdb8b744061
NayonSG <nayons.gomes@gmail.com>
2022-08-03 03:38:53 -0300

    Updates

---
+++ precache-manifest.2e62a99f0689bb3bc4fee6b70e761cf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78617c825ffea8a668f2",
+    "url": "/to-do-list/static/css/main.6243a8ec.chunk.css"
+  },
+  {
+    "revision": "78617c825ffea8a668f2",
+    "url": "/to-do-list/static/js/main.78617c82.chunk.js"
+  },
+  {
+    "revision": "9286b6dd4c5f9a73506c",
+    "url": "/to-do-list/static/js/1.9286b6dd.chunk.js"
+  },
+  {
+    "revision": "af0bf1497ad58103b57b",
+    "url": "/to-do-list/static/js/runtime~main.af0bf149.js"
+  },
+  {
+    "revision": "dfaaee8af7b2dacaa7adc7b1690a6daa",
+    "url": "/to-do-list/index.html"
+  }
+];
