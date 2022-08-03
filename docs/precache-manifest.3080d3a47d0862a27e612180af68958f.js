dfa44042da463ab2d4b17e9f4b43de1b0d645ade
NayonSG <nayons.gomes@gmail.com>
2022-08-03 02:45:38 -0300

    first commit

---
+++ docs/precache-manifest.3080d3a47d0862a27e612180af68958f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f384cf18d5bb1ec7aac6",
+    "url": "/static/css/main.6243a8ec.chunk.css"
+  },
+  {
+    "revision": "f384cf18d5bb1ec7aac6",
+    "url": "/static/js/main.f384cf18.chunk.js"
+  },
+  {
+    "revision": "9286b6dd4c5f9a73506c",
+    "url": "/static/js/1.9286b6dd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "90ea51457b9d3d9fad3b4c9429e7254d",
+    "url": "/index.html"
+  }
+];
